8545b09faf22859091a557fbfb2d06ed8cbb5f8d
san-smith <san-smith@mail.ru>
2020-04-04 11:53:15 +0700

    Add repos bloc

---
+++ lib/domain/bloc/repos_bloc.dart
@@ -0,0 +1,59 @@
+import 'package:bloc/bloc.dart';
+import 'package:mini_github_client/data/repository/user_data_repository.dart';
+import 'package:mini_github_client/domain/model/repository.dart';
+
+class ReposBloc extends Bloc<ReposEvent, ReposState> {
+  final UserDataRepository _userDataRepository;
+
+  ReposBloc(this._userDataRepository);
+
+  @override
+  ReposState get initialState => ReposInitState();
+
+  @override
+  Stream<ReposState> mapEventToState(ReposEvent event) async* {
+    if (event is ReposGetEvent) {
+      yield* _getReposToState(event.login);
+    }
+  }
+
+  Stream<ReposState> _getReposToState(String login) async* {
+    yield ReposLoadingState();
+    try {
+      var repos = await _userDataRepository.getRepos(login);
+      yield ReposReadyState(repos);
+    } catch (e) {
+      yield ReposErrorState(e.message);
+    }
+  }
+}
+
+/// states
+
+abstract class ReposState {}
+
+class ReposInitState extends ReposState {}
+
+class ReposLoadingState extends ReposState {}
+
+class ReposReadyState extends ReposState {
+  final List<Repository> user;
+
+  ReposReadyState(this.user);
+}
+
+class ReposErrorState extends ReposState {
+  final String message;
+
+  ReposErrorState(this.message);
+}
+
+/// events
+
+abstract class ReposEvent {}
+
+class ReposGetEvent extends ReposEvent {
+  final String login;
+
+  ReposGetEvent(this.login);
+}
--- lib/internal/dependencies/user_module.dart
+++ lib/internal/dependencies/user_module.dart
@@ -1,9 +1,16 @@
+import 'package:mini_github_client/domain/bloc/repos_bloc.dart';
 import 'package:mini_github_client/domain/bloc/user_bloc.dart';
 import 'package:mini_github_client/internal/dependencies/repository_module.dart';
 
 class UserModule {
   static UserBloc userBloc() {
     return UserBloc(
+      RepositoryModule.userRepository(),
+    );
+  }
+
+  static ReposBloc reposBloc() {
+    return ReposBloc(
       RepositoryModule.userRepository(),
     );
   }
